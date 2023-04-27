cce105739d7dcd1e89d8ad0166c79dc96da73756
Gagan <42744771+Gagan163264@users.noreply.github.com>
2023-04-27 11:26:54 +0530

    Update test.c

--- L2-YACC/test.c
+++ L2-YACC/test.c
@@ -10,12 +10,18 @@ int gcd(int u, int v)
 int main()
 {
     int k = 5+20/4;
-    typedef A;
     int a, b;
     printf("Enter first number: ");
     scanf("%d", &a);
     printf("Enter second number: ");
     scanf("%d", &b);
     printf("GCD is %d",gcd(a,b));
+    int a[10] = {1,2,4,0,0,0,0,0,0,0};
+    for(int i = 0; i<10; i++)
+    	a[i] = i||1;
+    do
+    {
+    	a[i] = ++i;
+    }while(i<20);
     return 1;
 }
